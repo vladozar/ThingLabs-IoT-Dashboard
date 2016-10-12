f6730b66d8e3c44b1407810f2fcf7d6c41654d44
Rita Zhang <rita.z.zhang@gmail.com>
2016-10-12 02:40:23 -0700

    Skip bad timestamp in graph

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.post('/:deviceId/led/:state', function (req, res) {
     var deviceId = req.params.deviceId;
     var ledState = req.params.state;   
     var messageData = '{"ledState":' + ledState + '}';
-    console.log(messageData);
     var client = IotHubClient.fromConnectionString(iotHubConnectionString);
     client.open(function (err) {
         if (err) {
